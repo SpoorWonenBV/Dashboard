c756bc5fa67f86979a0349d2aa05d25de53c02d6
josh01021 <joshvermeijden@gmail.com>
2026-07-28 10:33:04 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME='vastgoed-dashboard-static-v40-4';
+const CACHE_NAME='vastgoed-dashboard-static-v40-5';
 const OFFLINE_URL='/offline.html';
 const STATIC_PATHS=new Set([
   '/style.css',
